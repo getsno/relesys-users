d301333524b246dab729f51c8500ab039481586a
Anton Samofal <devtosha@gmail.com>
2023-03-11 20:54:21 +0100

    Add credentials to config

--- config/config.php
+++ config/config.php
@@ -1,8 +1,16 @@
 <?php
 
 /*
- * You can place your custom package configuration in here.
- */
+|--------------------------------------------------------------------------
+| Relesys API OAuth2 credentials
+|--------------------------------------------------------------------------
+|
+| The API uses a OAuth2 token (Bearer token) for authentication.
+| To get one, we must first make an HTTP call to our token endpoint,
+| with the credentials provided by Relesys prior to this.
+|
+*/
 return [
-
+    'relesys_client_id'     => env('RELESYS_CLIENT_ID'),
+    'relesys_client_secret' => env('RELESYS_CLIENT_SECRET'),
 ];
