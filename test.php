2b29c50adb28f6c9b0e142071a81c05d7aa45ae2
Anton Samofal <devtosha@gmail.com>
2023-03-20 14:29:14 +0100

    Remove test file

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function test($str, array $constructor = [], Closure $mock = null) {
-    // var_dump(...func_get_args());
-    var_dump(...array_filter(func_get_args()));
-}
-
-test('string', [], fn() => 'lol');
-
